71a2d60c16ed8722f2a44e6446a6331f3bcbdc17
Codinglynd <ethanrocks500@proton.me>
2025-07-31 10:33:59 -0500

    commit

--- learning python/Other/cat.py
+++ learning python/Other/cat.py
@@ -1,7 +1,7 @@
 # asks "how many times should this cat meow? " and puts that answer into the variable "i" as an integer
 i = int(input("How many times does the cat say meow? "))
 
-# says "while i is not equal to 0 say "meow" than subtract 1 from variable i" a
+# says "while i is not equal to 0 say "meow" than subtract 1 from variable i"
 while i != 0:
     print("meow")
     i = i - 1
