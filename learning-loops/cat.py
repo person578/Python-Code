a49ac04ee98ab1e38170ce5307a8571e54c940fe
Codinglynd <ethanrocks500@proton.me>
2025-07-31 17:52:37 -0500

    commit

--- learning-loops/cat.py
+++ learning-loops/cat.py
@@ -1,4 +1,4 @@
-# a
+# abc
 # asks "how many times should this cat meow? " and puts that answer into the variable "i" as an integer
 i = int(input("How many times do you want to make the cat meow? "))
 
