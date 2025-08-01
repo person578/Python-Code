267f7058d41b4d78678f0fab082bf5040d9675cd
Codinglynd <ethanrocks500@proton.me>
2025-08-01 13:21:09 -0500

    commit

--- learning-loops/cat-funtion.py
+++ learning-loops/cat-funtion.py
@@ -1,6 +1,17 @@
 def main():
-    meow(3)
+    number = get_number()
+    meow()
+
+def get_number():
+    while True:
+        n = int(input("How many times does the cat meow? "))
+        if n > 0:
+            return n
+
 
 def meow(n):
     for _ in range(n):
         print("meow")
+
+
+main()
