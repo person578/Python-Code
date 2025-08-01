b920e8ae18bb0b04c4ab070d3734ae9522550378
Codinglynd <ethanrocks500@proton.me>
2025-07-31 19:58:14 -0500

    commit

--- learning-loops/cat.py
+++ learning-loops/cat.py
@@ -1,5 +1,5 @@
 # asks "how many times should this cat meow? " and puts that answer into the variable "i" as an integer
-i = int(input("How many times do you want to make the cat meow? "))
+i = int(input("How many times does the cat meow? "))
 
 # says "while i is not equal to 0 say "meow" than subtract 1 from variable i"
 while i != 0:
