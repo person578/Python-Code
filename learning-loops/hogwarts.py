76ba698563862c426387d42b7c13cd607a1c3b8c
Codinglynd <ethanrocks500@proton.me>
2025-08-01 21:58:04 -0500

    commit

--- learning-loops/hogwarts.py
+++ learning-loops/hogwarts.py
@@ -5,7 +5,5 @@ students = {
     "Draco": "Slytherin",
     }
 
-print(students["Hermione"])
-print(students["Harry"])
-print(students["Ron"])
-print(students["Draco"])
+for student in students:
+    print(student)
