0193335e8bb3c3a49813319a6e8ee15bcb0ac145
Codinglynd <ethanrocks500@proton.me>
2025-08-04 23:25:45 -0500

    commit

---
+++ random-stuff/comparer.py
@@ -0,0 +1,11 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+if x < y:
+    print("x is less then y")
+
+elif x > y:
+    print("x is more then y")
+
+else:
+    print("x equals y")
