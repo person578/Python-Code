7302276c24f92d412ded8be3aff751887e91a5d9
person578 <ethanrocks500@proton.me>
2025-12-19 15:00:25 -0600

    commit

--- Pandas/DataFrames.py
+++ Pandas/DataFrames.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
 
+data = {"Name": ["Ethan", "Ash", "John", "Tom"],
+        "Age": [21, 27, 34, 40]}
+df = pd.DataFrame(data, index=["Employee 1", "Employee 2", "Employee 3", "Employee 4"])
+print(df)
