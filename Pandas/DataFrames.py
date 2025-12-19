18c8ecb7b42c57159be3b9ec92fac6674f70508d
person578 <ethanrocks500@proton.me>
2025-12-19 15:08:48 -0600

    commit

--- Pandas/DataFrames.py
+++ Pandas/DataFrames.py
@@ -4,4 +4,5 @@ import numpy as np
 data = {"Name": ["Ethan", "Ash", "John", "Tom"],
         "Age": [21, 27, 34, 40]}
 df = pd.DataFrame(data, index=["Employee 1", "Employee 2", "Employee 3", "Employee 4"])
-print(df)
+
+print(df.iloc[1])
