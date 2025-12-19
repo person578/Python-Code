ff5b1e8b33a8868dfdb073019882879d4e4b3aaf
person578 <ethanrocks500@proton.me>
2025-12-19 15:12:01 -0600

    commit

--- Pandas/DataFrames.py
+++ Pandas/DataFrames.py
@@ -5,4 +5,7 @@ data = {"Name": ["Ethan", "Ash", "John", "Tom"],
         "Age": [21, 27, 34, 40]}
 df = pd.DataFrame(data, index=["Employee 1", "Employee 2", "Employee 3", "Employee 4"])
 
-print(df.iloc[1])
+# Add a new column
+df["Job"] = ["Data Analyst", "Data Engineer", "Data Manager", "Software Engineer"]
+
+print(df)
