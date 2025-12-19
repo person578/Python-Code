d114a65c80ce3199f53a0dfccdc132cdc6bf280e
person578 <ethanrocks500@proton.me>
2025-12-19 12:30:21 -0600

    Added new number in data array

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 # A series is like a single column in excel
 
-data = [100, 102, 104, 200, 202]
+data = [100, 102, 104, 200, 202, 204]
 # data = [100.1, 102.2, 104.3]
 # data = ["A", "B", "C"]
 # data = [True, False, True]
 
-series = pd.Series(data, index=["A", "B", "C", "D", "E"])
+series = pd.Series(data, index=["A", "B", "C", "D", "E", "F"])
 
-print(series)
+print(series[series >= 200])
