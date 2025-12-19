575890beadf158ac639919e6c02f48dd1defca5a
person578 <ethanrocks500@proton.me>
2025-12-19 14:47:55 -0600

    commit

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import numpy as np
+
+calories = {"Day 1": 1750, "Day 2": 2100, "Day 3": 1700}
+
+series = pd.Series(calories)
+
+print(series[series >= 2000])
 
 # A series is like a single column in excel
 
-data = [100, 102, 104, 200, 202, 204]
+#data = np.array([100, 102, 104, 200, 202, 204])
 # data = [100.1, 102.2, 104.3]
 # data = ["A", "B", "C"]
 # data = [True, False, True]
 
-series = pd.Series(data, index=["A", "B", "C", "D", "E", "F"])
+#series = pd.Series(data, index=["A", "B", "C", "D", "E", "F"])
 
-print(series[series >= 200])
+#print(series[series < 200])
