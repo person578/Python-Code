b364b44de026f2f95fc68fbeefe4116a9bf6f525
person578 <ethanrocks500@proton.me>
2025-12-19 12:24:24 -0600

    Added file for learning how series work in pandas

---
+++ Pandas/Series.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# A series is like a single column in excel
+
+data = [100, 102, 104, 200, 202]
+# data = [100.1, 102.2, 104.3]
+# data = ["A", "B", "C"]
+# data = [True, False, True]
+
+series = pd.Series(data, index=["A", "B", "C", "D", "E"])
+
+print(series)
